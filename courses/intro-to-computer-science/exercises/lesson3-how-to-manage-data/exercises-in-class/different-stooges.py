b1a36f0c72d649a869883af4e1c4eda3a344dcff
Felipe Oliveira <felipe.o.silva18@gmail.com>
2016-01-29 09:41:30 -0200

    adding news exercises from the Lesson 3

---
+++ courses/intro-to-computer-science/exercises/lesson3-how-to-manage-data/exercises-in-class/different-stooges.py
@@ -0,0 +1,19 @@
+# We defined:
+
+stooges = ['Moe','Larry','Curly']
+print stooges
+
+# but in some Stooges films, Curly was
+# replaced by Shemp.
+
+# Write one line of code that changes
+# the value of stooges to be:
+
+stooges_new = ['Moe','Larry','Shemp']
+stooges[2] = stooges_new[2]
+
+print stooges
+
+# but does not create a new List
+# object.
+
---
+++ courses/intro-to-computer-science/exercises/lesson3-how-to-manage-data/exercises-in-class/measure-udacity.py
@@ -0,0 +1,19 @@
+# Define a procedure, measure_udacity,
+# that takes as its input a list of strings,
+# and returns a number that is a count
+# of the number of elements in the input
+# list that start with the uppercase 
+# letter 'U'.
+
+def measure_udacity(strings):
+    count = 0
+    for current in strings:
+        if current[0] == 'U':
+            count += 1
+    return count
+        
+print measure_udacity(['Dave','Sebastian','Katy'])
+#>>> 0
+
+print measure_udacity(['Umika','Umberto'])
+#>>> 2
